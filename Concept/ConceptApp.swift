bc68be21be3f7aead49f91e6f87c2fa6c50ec3e5
tylertorres <tylertorresp@gmail.com>
2023-12-30 21:18:17 -0500

    Initial Commit

---
+++ Concept/ConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConceptApp.swift
+//  Concept
+//
+//  Created by Tyler Torres on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
